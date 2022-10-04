3fc1041e760338ca5ce7e16373289d5924fe3cef
ssosag <santiago.sg003@gmail.com>
2022-10-03 19:51:54 -0500

    taller6

---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,40 @@
+package vehiculos;
+import java.util.ArrayList;
+
+public class Pais {
+	String nombre;
+	
+	public Pais(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public Pais paisMasVendedor() {
+		ArrayList<Pais> listado = Vehiculo.arrayPaises;
+		int n = listado.size();
+	    int maxcount = 0;
+	    Pais paisMasRepetido = null;
+	    for (int i = 0; i < n; i++) {
+	      int count = 0;
+	      for (int j = 0; j < n; j++) {
+	        if (listado.get(i) == listado.get(j)) {
+	          count++;
+	        }
+	      }
+	 
+	      if (count > maxcount) {
+	        maxcount = count;
+	        paisMasRepetido = listado.get(i);
+	      }
+	    }
+	 
+	    return paisMasRepetido;
+	}
+}
